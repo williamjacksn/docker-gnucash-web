03b83f4c25898d4137c06aa5e0befae293cd19a0
William Jackson <565174+williamjacksn@users.noreply.github.com>
2023-05-30 20:52:40 -0500

    Log with notch

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import gnucash_web.wsgi
+import notch
 import signal
 import sys
 import waitress
+
+log = notch.make_log('docker_gnucash_web')
 
 def handle_sigterm(_signal, _frame):
     sys.exit()
